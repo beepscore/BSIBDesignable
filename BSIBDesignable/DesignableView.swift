8d2a3993ba9f71990c270537f8929a2e49baf1d1
beepscore <support@beepscore.com>
2016-01-29 15:17:48 -0800

    Add DesignableView as subclass of UIView

---
+++ BSIBDesignable/DesignableView.swift
@@ -0,0 +1,21 @@
+//
+//  DesignableView.swift
+//  BSIBDesignable
+//
+//  Created by Steve Baker on 1/29/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class DesignableView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
