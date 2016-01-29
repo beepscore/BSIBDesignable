d65703efede7b2afe8b8873f77ccb663df76710d
beepscore <support@beepscore.com>
2016-01-29 15:46:15 -0800

    In DesignableView borderColor add comment

--- BSIBDesignable/DesignableView.swift
+++ BSIBDesignable/DesignableView.swift
@@ -25,6 +25,12 @@ class DesignableView: UIView {
 
     @IBInspectable var borderColor: UIColor? {
         didSet {
+            // runtime attribute supports type UIColor but not CGColor
+            // so method argument type is UIColor.
+            // layer.borderColor type is CGColor.
+            // So convert argument UIColor to CGColor for use by layer.
+            // This is an example of a computed property.
+            // http://nshipster.com/ibinspectable-ibdesignable/ 
             layer.borderColor = borderColor?.CGColor
         }
     }
