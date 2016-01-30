0654a9ee12bcc28a40620c23adee02d84df73bce
beepscore <support@beepscore.com>
2016-01-29 17:59:52 -0800

    For clarity extract String extension from CalculatorView to new file String+Drawing.swift.
    Note Jake Lin similarly extracted to file String+IBCalculator.swift
    https://github.com/JakeLin/IBCalculator/blob/master/IBCalculator/String%2BIBCalculator.swift

--- BSIBDesignable/CalculatorView.swift
+++ BSIBDesignable/CalculatorView.swift
@@ -6,47 +6,6 @@
 
 import UIKit
 
-/// The alignment for drawing an String inside a bounding rectangle.
-enum NCStringAlignment {
-    case LeftTop
-    case CenterTop
-    case RightTop
-    case LeftCenter
-    case Center
-    case RightCenter
-    case LeftBottom
-    case CenterBottom
-    case RightBottom
-}
-
-extension String {
-    /// Draw the `String` inside the bounding rectangle with a given alignment.
-    func drawAtPointInRect(rect: CGRect, withAttributes attributes: [String: AnyObject]?, andAlignment alignment: NCStringAlignment) {
-        let size = self.sizeWithAttributes(attributes)
-        let x, y: CGFloat
-        
-        switch alignment {
-        case .LeftTop, .LeftCenter, .LeftBottom:
-            x = CGRectGetMinX(rect)
-        case .CenterTop, .Center, .CenterBottom:
-            x = CGRectGetMidX(rect) - size.width / 2
-        case .RightTop, .RightCenter, .RightBottom:
-            x = CGRectGetMaxX(rect) - size.width
-        }
-        
-        switch alignment {
-        case .LeftTop, .CenterTop, .RightTop:
-            y = CGRectGetMinY(rect)
-        case .LeftCenter, .Center, .RightCenter:
-            y = CGRectGetMidY(rect) - size.height / 2
-        case .LeftBottom, .CenterBottom, .RightBottom:
-            y = CGRectGetMaxY(rect) - size.height
-        }
-        
-        self.drawAtPoint(CGPoint(x: x, y: y), withAttributes: attributes)
-    }
-}
-
 extension CGRect {
     /// Convert a rect to a pixel-aligned version, rounding position and size
     func pixelAligned() -> CGRect {
---
+++ BSIBDesignable/String+Drawing.swift
@@ -0,0 +1,51 @@
+//
+//  String+Drawing.swift
+//  BSIBDesignable
+//
+//  Created by Steve Baker on 1/29/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+/// The alignment for drawing an String inside a bounding rectangle.
+enum NCStringAlignment {
+    case LeftTop
+    case CenterTop
+    case RightTop
+    case LeftCenter
+    case Center
+    case RightCenter
+    case LeftBottom
+    case CenterBottom
+    case RightBottom
+}
+
+extension String {
+    /// Draw the `String` inside the bounding rectangle with a given alignment.
+    func drawAtPointInRect(rect: CGRect, withAttributes attributes: [String: AnyObject]?, andAlignment alignment: NCStringAlignment) {
+        let size = self.sizeWithAttributes(attributes)
+        let x, y: CGFloat
+        
+        switch alignment {
+        case .LeftTop, .LeftCenter, .LeftBottom:
+            x = CGRectGetMinX(rect)
+        case .CenterTop, .Center, .CenterBottom:
+            x = CGRectGetMidX(rect) - size.width / 2
+        case .RightTop, .RightCenter, .RightBottom:
+            x = CGRectGetMaxX(rect) - size.width
+        }
+        
+        switch alignment {
+        case .LeftTop, .CenterTop, .RightTop:
+            y = CGRectGetMinY(rect)
+        case .LeftCenter, .Center, .RightCenter:
+            y = CGRectGetMidY(rect) - size.height / 2
+        case .LeftBottom, .CenterBottom, .RightBottom:
+            y = CGRectGetMaxY(rect) - size.height
+        }
+        
+        self.drawAtPoint(CGPoint(x: x, y: y), withAttributes: attributes)
+    }
+    
+}
