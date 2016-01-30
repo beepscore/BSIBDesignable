fda563fdea376fe4eb3dccb65cd3bb00605fde9d
beepscore <support@beepscore.com>
2016-01-29 22:43:25 -0800

    Add StringyView.swift

---
+++ BSIBDesignable/StringyView.swift
@@ -0,0 +1,21 @@
+//
+//  StringyView.swift
+//  BSIBDesignable
+//
+//  Created by Steve Baker on 1/29/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+import UIKit
+
+class StringyView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
